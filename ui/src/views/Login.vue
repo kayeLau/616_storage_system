92e77b16bed607e0935703d88f3e561d83739bc5
kayeLau <66953394+kayeLau@users.noreply.github.com>
2024-10-19 18:43:44 +0800

    feature: new login page

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -2,6 +2,7 @@
     <div id="login">
         <div class="container" id="container">
             <div class="form-container sign-in">
+                <img src="../assets/616_logo.png" alt="616_logo" class='logo'>
                 <div class="login-form">
                     <h1>Sign In</h1>
                     <input type="text" placeholder="用戶名稱" v-model="loginInfo.userName" @keydown.enter="toLogin(false)">
@@ -11,7 +12,7 @@
             </div>
             <div class="toggle-container">
                 <div class="toggle">
-                    <div class="toggle-panel toggle-right">
+                    <div class="toggle-panel">
                         <h1>616智能倉務系統</h1>
                     </div>
                 </div>
@@ -82,6 +83,17 @@ function toHome() {
 
 </script>
 <style>
+@media screen and (max-width: 750px) {
+#container {
+    flex-direction: column;
+    width: 80vw;
+  }
+}
+.logo{
+    padding-top: 10px;
+    padding-left: 10px;
+}
+
 #login {
     background-color: #c9d6ff;
     background: linear-gradient(to right, #e2e2e2, #c9d6ff);
@@ -90,35 +102,15 @@ function toHome() {
     justify-content: center;
     flex-direction: column;
     height: 100vh;
+    width: 100vw;
 }
 
 .container {
     background-color: #fff;
-    border-radius: 30px;
     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.35);
-    position: relative;
-    overflow: hidden;
     width: 768px;
-    max-width: 100%;
     min-height: 480px;
-}
-
-.container p {
-    font-size: 14px;
-    line-height: 20px;
-    letter-spacing: 0.3px;
-    margin: 20px 0;
-}
-
-.container span {
-    font-size: 12px;
-}
-
-.container a {
-    color: #333;
-    font-size: 13px;
-    text-decoration: none;
-    margin: 15px 0 10px;
+    display: flex;
 }
 
 .container button {
@@ -135,11 +127,6 @@ function toHome() {
     cursor: pointer;
 }
 
-.container button.hidden {
-    background-color: transparent;
-    border-color: #fff;
-}
-
 .container .login-form {
     background-color: #fff;
     display: flex;
@@ -147,7 +134,7 @@ function toHome() {
     justify-content: center;
     flex-direction: column;
     padding: 0 40px;
-    height: 100%;
+    height: calc(100% - 50px);
 }
 
 .container input {
@@ -162,81 +149,15 @@ function toHome() {
 }
 
 .form-container {
-    position: absolute;
-    top: 0;
     height: 100%;
-    transition: all 0.6s ease-in-out;
-}
-
-.sign-in {
-    left: 0;
-    width: 50%;
-    z-index: 2;
-}
-
-.container.active .sign-in {
-    transform: translateX(100%);
-}
-
-.sign-up {
-    left: 0;
-    width: 50%;
-    opacity: 0;
-    z-index: 1;
-}
-
-.container.active .sign-up {
-    transform: translateX(100%);
-    opacity: 1;
-    z-index: 5;
-    animation: move 0.6s;
-}
-
-@keyframes move {
-
-    0%,
-    49.99% {
-        opacity: 0;
-        z-index: 1;
-    }
-
-    50%,
-    100% {
-        opacity: 1;
-        z-index: 5;
-    }
-}
-
-.social-icons {
-    margin: 20px 0;
-}
-
-.social-icons a {
-    border: 1px solid #ccc;
-    border-radius: 20%;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 3px;
-    width: 40px;
-    height: 40px;
+    overflow: hidden;
 }
 
 .toggle-container {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 50%;
-    height: 100%;
+    flex: 1;
     overflow: hidden;
-    transition: all 0.6s ease-in-out;
     border-radius: 150px 0 0 100px;
     z-index: 1000;
-}
-
-.container.active .toggle-container {
-    transform: translateX(-100%);
-    border-radius: 0 150px 100px 0;
 }
 
 .toggle {
@@ -244,47 +165,15 @@ function toHome() {
     height: 100%;
     background: linear-gradient(to right, #5c6bc0, #512da8);
     color: #fff;
-    position: relative;
-    left: -100%;
-    height: 100%;
-    width: 200%;
-    transform: translateX(0);
-    transition: all 0.6s ease-in-out;
-}
-
-.container.active .toggle {
-    transform: translateX(50%);
 }
 
 .toggle-panel {
-    position: absolute;
-    width: 50%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
     padding: 0 30px;
     text-align: center;
-    top: 0;
-    transform: translateX(0);
-    transition: all 0.6s ease-in-out;
 }
 
-.toggle-left {
-    transform: translateX(-200%);
-}
-
-.container.active .toggle-left {
-    transform: translateX(0);
-}
-
-.toggle-right {
-    right: 0;
-    transform: translateX(0);
-}
-
-.container.active .toggle-right {
-    transform: translateX(200%);
-}
 </style>
